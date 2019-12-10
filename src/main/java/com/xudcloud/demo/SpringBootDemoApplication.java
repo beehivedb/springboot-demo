7eee9722009796678eddff1a5ba29cf8240d1563
majingru <majingru@oneapm.com>
2019-12-10 17:47:34 +0800

    add actuator

--- src/main/java/com/xudcloud/demo/SpringBootDemoApplication.java
+++ src/main/java/com/xudcloud/demo/SpringBootDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbooDemoApplication {
+public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbooDemoApplication.class, args);
+		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
 }
